0434c46e6ea9952dfe1802cc0ad7ac62a61f5cb3
Saesha Agarwal <saesha@saeshas-mbp-2.attlocal.net>
2020-10-24 10:55:15 -0400

    Adding docker file

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -20,6 +20,20 @@ export class CustomersComponent implements OnInit {
   displayModeEnum = DisplayModeEnum;
   totalRecords = 0;
   pageSize = 10;
+  
+  players = [
+    {id: 1, playerName: 'Cristiano Ronaldo'},
+    {id: 2, playerName: 'Lionel Messi'},
+    {id: 3, playerName: 'Neymar Jr'},
+    {id: 4, playerName: 'Toni Kroos'},
+    {id: 5, playerName: 'Luiz Suarez', disabled: true},
+    {id: 6, playerName: 'Karim Benzema'},
+    {id: 7, playerName: 'Eden Hazard'},
+  ];
+  selected = [
+    {id: 2, playerName: 'Toni Kroos'}
+  ];
+
   mapComponentRef: ComponentRef<any>;
   _filteredCustomers: ICustomer[] = [];
 
