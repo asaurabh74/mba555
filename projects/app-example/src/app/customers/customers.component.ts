1e7de607d58ba9279eeea7993ff0d5a0f05da6d0
Saurabh Agarwal <asaurabh@yahoo.com>
2020-11-15 18:11:39 -0500

    adding search filters

--- projects/app-example/src/app/customers/customers.component.ts
+++ projects/app-example/src/app/customers/customers.component.ts
@@ -42,9 +42,7 @@ export class CustomersComponent implements OnInit {
   ]
 
   
-  selected = [
-    
-  ];
+  selected = [];
 
   mapComponentRef: ComponentRef<any>;
   _filteredCustomers: ICustomer[] = [];
